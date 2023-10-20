81fdfb7951765d70f3d8debd0d09bce04bc97114
restrepoTorres <vmanuel.restrepo@udea.edu.co>
2023-10-20 04:01:11 -0500

    feat: add Firebase to the project

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,22 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyB13aXQX87SvCK9U1PhGMRE3r3toUDJfbI",
+  authDomain: "dnd2-e4297.firebaseapp.com",
+  projectId: "dnd2-e4297",
+  storageBucket: "dnd2-e4297.appspot.com",
+  messagingSenderId: "600615642544",
+  appId: "1:600615642544:web:c372b0b6d66df4e73ce275",
+  measurementId: "G-9ZX3XWYK0P"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 function App() {
   const [count, setCount] = useState(0)
 
@@ -16,7 +31,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Aquí se va a construir DnD2</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
