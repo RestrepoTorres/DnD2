33cef201e8a4a0770934ba8465f3e52049194da0
restrepoTorres <vmanuel.restrepo@udea.edu.co>
2023-10-26 00:36:32 -0500

    feat: basic login funcionallity implemented

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,8 +2,8 @@ import { Route, Routes, Link } from "react-router-dom";
 
 export const Button = ({ to, text, handleClick }) => {
   return (
-    <Link to={to}>
+    
       <button onClick={handleClick}>{text}</button>
-    </Link>
+    
   );
 };
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,41 +2,34 @@ import { Button } from "../components/Button";
 import { firebaseToken } from "../components/FireBaseToken";
 import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 import { initializeApp } from "firebase/app";
+import { Route, Routes, Link, useNavigate } from "react-router-dom";
+import { HomePage } from "../pages/HomePage";
 
 const firebaseConfig = firebaseToken;
 const provider = new GoogleAuthProvider();
-
 const app = initializeApp(firebaseConfig);
 const auth = getAuth();
-function googleLogin() {
-  signInWithPopup(auth, provider)
-    .then((result) => {
-      // This gives you a Google Access Token. You can use it to access the Google API.
-      const credential = GoogleAuthProvider.credentialFromResult(result);
-      const token = credential.accessToken;
-      // The signed-in user info.
-      const user = result.user;
 
-      //console.log(result.user);
-      // IdP data available using getAdditionalUserInfo(result)
-      // ...
-    })
-    .catch((error) => {
-      // Handle Errors here.
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      // The email of the user's account used.
-      const email = error.customData.email;
-      // The AuthCredential type that was used.
-      const credential = GoogleAuthProvider.credentialFromError(error);
-      // ...
-    });
-}
 export const Login = () => {
+  const navigate = useNavigate();
+
+  async function googleLogin() {
+    const result = await signInWithPopup(auth, provider);
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    const credential = GoogleAuthProvider.credentialFromResult(result);
+    const token = credential.accessToken;
+    // The signed-in user info.
+    const user = result.user;
+    //console.log(result.user);
+    // IdP data available using getAdditionalUserInfo(result)
+    // ...
+    navigate("/character-selector");
+  }
+
   return (
     <>
       <h1>Logeate o vete</h1>
-      <Button to="/character-selector" text="log with Google" handleClick={googleLogin} />
+      <Button text="log with Google" handleClick={googleLogin} />
     </>
   );
 };
