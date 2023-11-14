6abd70804fd91e42eabf794fa4226641c819501e
restrepoTorres <vmanuel.restrepo@udea.edu.co>
2023-11-13 22:43:27 -0500

    feat: force matchmaking function

--- src/firebase_back/matchmaking.js
+++ src/firebase_back/matchmaking.js
@@ -1,8 +1,23 @@
+import EloRank from "elo-rank";
 
+var elo = new EloRank();
+var playerA = 1200;
+var playerB = 1245;
 
+//Gets expected score for first parameter
+var expectedScoreA = elo.getExpected(playerA, playerB);
+var expectedScoreB = elo.getExpected(playerB, playerA);
 
-export function getIntervals(player){
+console.log(expectedScoreA);
+
+export function correctInterval(winrate) {
+  var e = 1 - winrate;
+  var aux = 1 / e - 1;
+
+  return 400 * Math.log10(aux);
+}
+
+export function searchRivals() {
     
-
-    return
+    return 2;
 }
