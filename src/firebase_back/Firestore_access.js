a99ae7154eb7c7a7a625e84acea5ff9c0337763a
restrepoTorres <vmanuel.restrepo@udea.edu.co>
2023-11-20 08:45:20 -0500

    refactor: separated file for add dummy data to db

--- src/firebase_back/Firestore_access.js
+++ src/firebase_back/Firestore_access.js
@@ -1,5 +1,3 @@
-import fakedata from "./MOCK_DATA.json";
-import races from "./Races.json";
 import { firebaseToken } from "./FireBaseToken";
 import { initializeApp } from "firebase/app";
 import {
@@ -86,55 +84,4 @@ export async function afterMach(id, points, win) {
     },
     { merge: true }
   );
-}
-
-export function dumpFakeData() {
-  async function addFakeUser(name, uid) {
-    await setDoc(doc(db, "users", "" + uid), {
-      name: name,
-    });
-  }
-
-  async function addFakeCharacter(
-    name,
-    elo,
-    gamesPlayed,
-    wins,
-    winRate,
-    id,
-    avatar
-  ) {
-    await setDoc(doc(db, "characters", "" + id), {
-      nick: name,
-      elo: elo,
-      gamesPlayed: gamesPlayed,
-      wins: wins,
-      userId: id,
-      winRate: winRate,
-      avatar: avatar,
-    });
-  }
-  fakedata.forEach((doc) => {
-    addFakeCharacter(
-      doc.nickName,
-      doc.elo,
-      doc.gamesPlayed,
-      doc.wins,
-      doc.wins / doc.gamesPlayed,
-      doc.id,
-      doc.avatar
-    );
-    addFakeUser(doc.name, doc.id);
-  });
-}
-
-async function addRaces() {
-  races.forEach((docu) => {
-    setDoc(doc(db, "races", docu.nombre), {
-      name: docu.nombre,
-      description: docu.descripcion,
-      habilities: docu.habilidades,
-      stats: docu.estadisticas_base,
-    });
-  });
 }
