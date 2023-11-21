d432572574a06d7978a33181e6af98a70517f468
restrepoTorres <vmanuel.restrepo@udea.edu.co>
2023-11-20 20:31:50 -0500

    feat: frontend class selector finished

--- src/pages/CharacterCreator.jsx
+++ src/pages/CharacterCreator.jsx
@@ -22,12 +22,22 @@ async function formOnClick(event, navigate) {
 
 export const CharacterCreator = () => {
   const navigate = useNavigate();
-  const [race, setRace] = useState({ description: "Select a race" });
+  const [race, setRace] = useState({
+    name: "High Elf",
+    description: "Maestros en el arte de la magia arcana.",
+    habilities: ["Magia arcana avanzada", "Teletransportación"],
+    stats: {
+      fuerza: 4,
+      destreza: 6,
+      inteligencia: 9,
+      vida: 90,
+    },
+  });
 
   return (
     <>
       <Header />
-      <div id ="charactercreation">
+      <div id="charactercreation">
         <Article>
           <h1>Character Selector </h1>
           <form
@@ -44,7 +54,20 @@ export const CharacterCreator = () => {
               autoComplete="given-name"
             />
             <br />
-            <label>Choose a race for your character:</label>
+            <label>Elo:</label>
+            <br />
+            <input type="number" name="Elo" id="elo" required placeholder="4" />
+            <br />
+            <label>games played:</label>
+            <br />
+            <input type="number" name="gamesPlayed" required placeholder="4" />
+            <br />
+            <label>Won games:</label>
+            <br />
+            <input type="number" name="wonGames" required placeholder="4" />
+            <br />
+
+            <label>Choose a race:</label>
             <br />
             <select
               name="race"
@@ -60,19 +83,22 @@ export const CharacterCreator = () => {
               <option>Human</option>
               <option>Orc</option>
             </select>
-
             <br />
-            <label>Elo:</label>
+            <label>Choose a class:</label>
             <br />
-            <input type="number" name="Elo" id="elo" required placeholder="4" />
-            <br />
-            <label>games played:</label>
-            <br />
-            <input type="number" name="gamesPlayed" required placeholder="4" />
-            <br />
-            <label>Won games:</label>
-            <br />
-            <input type="number" name="wonGames" required placeholder="4" />
+            <select
+              name="Class"
+              //onChange={async () =>
+              //</form>setRace(
+              //await getDocument("races", document.forms.RegForm.race.value)
+              //)
+              //}
+            >
+              <option>Warrior</option>
+              <option>Rogue</option>
+              <option>Mage</option>
+              <option>Marksman</option>
+            </select>
             <br />
             <br />
             <input type="submit" value="Create player"></input>
@@ -80,7 +106,14 @@ export const CharacterCreator = () => {
         </Article>
 
         <Article>
-          <p>{race.description}</p>
+          <h1>{race.name}</h1>
+          <p>Descripción : {race.description}</p>
+          <p>habilidad principal: {race.habilities[0]}</p>
+          <p>habilidad secundaria: {race.habilities[1]}</p>
+          <p>fuerza = {race.stats["fuerza"]}</p>
+          <p>inteligencia = {race.stats["inteligencia"]}</p>
+          <p>destreza = {race.stats["destreza"]}</p>
+          <p>vida = {race.stats["vida"]}</p>
         </Article>
       </div>
       <Footer />
